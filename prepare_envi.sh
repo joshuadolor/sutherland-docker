209234dfd80c6a0e8bcb7a63e4cb39638064255d
Joshua Dolor <dolorjsh@gmail.com>
2021-10-05 02:16:15 +0800

    nginx configs

--- prepare_envi.sh
+++ prepare_envi.sh
@@ -2,7 +2,10 @@
 source slgvars.env
 #replace variables using slgvars.env
 envsubst < templates/.env.conf > .env
-envsubst < templates/nginx.conf > reverse-proxy/nginx.conf
+envsubst < templates/nginx/nginx.conf > reverse-proxy/nginx.conf
+envsubst < templates/nginx/upstream/loi.conf > reverse-proxy/upstream/loi.conf
+envsubst < templates/nginx/upstream/ofac.conf > reverse-proxy/upstream/ofac.conf
+envsubst < templates/nginx/upstream/slg.conf > reverse-proxy/upstream/slg.conf
 
 # if (( "$1" == "deploy"));
 # then 
