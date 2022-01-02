0116ada0f173de29fbbb40d7a5ec26007374982d
zjoshua.dolor <jdolor@eservicios.indracompany.com>
2022-01-03 02:18:51 +0800

    update

--- prepare_envi.sh
+++ prepare_envi.sh
@@ -6,6 +6,7 @@ envsubst < templates/nginx/nginx.conf > reverse-proxy/nginx.conf
 envsubst < templates/nginx/upstream/loi.conf > reverse-proxy/upstream/loi.conf
 envsubst < templates/nginx/upstream/ofac.conf > reverse-proxy/upstream/ofac.conf
 envsubst < templates/nginx/upstream/slg.conf > reverse-proxy/upstream/slg.conf
+envsubst < templates/nginx/upstream/onboarding.conf > reverse-proxy/upstream/onboarding.conf
 
 # if (( "$1" == "deploy"));
 # then 
